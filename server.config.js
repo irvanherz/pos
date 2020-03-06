5bc728994a204dd2450704b22c58fd84d0f38d9e
irvanherz <irvan.herz@gmail.com>
2020-03-06 09:16:15 +0700

    Fix error reporting problems

--- server.config.js
+++ server.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps : [{
-    name: ‘POS App’,
-    script: ‘index.js’,
-    instances: ‘max’,
-    max_memory_restart: ‘256M’,
+    name: 'POS App',
+    script: 'index.js',
+    instances: 'max',
+    max_memory_restart: '256M',
     env: {
-      NODE_ENV: ‘development’
+      NODE_ENV: 'development'
     },
     env_production: {
-      NODE_ENV: ‘production’
+      NODE_ENV: 'production'
     }
   }]
 };
