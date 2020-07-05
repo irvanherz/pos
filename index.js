d6ebda88b7695d98e4910ce00b7f298d1e018614
irvanherz <irvan.herz@gmail.com>
2020-07-06 04:33:24 +0700

    tambah doc

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 const routeBase = require('./src/routes/index')
 const cors = require('cors')
 const helper = require('./src/helper/')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 var corsOptions = {
 	origin: 'http://localhost:3000'
@@ -16,6 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'))
 app.use('/assets',express.static('./uploads'))
 app.use('/', cors(corsOptions), routeBase)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 const server = app.listen(3001, '127.0.0.1', () => {
 	console.log('Listening on 127.0.0.1:3001')
